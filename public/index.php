f0e088fd43bf704f7265839f174c2c30adbc5f25
Jonas Poli <jonaspoli@gmail.com>
2026-06-30 16:48:43 -0300

    Ajustes

--- public/index.php
+++ public/index.php
@@ -1,5 +1,17 @@
 <?php
 
+if (!function_exists('ignore_user_abort')) {
+    function ignore_user_abort(?bool $enable = null): int {
+        return 0;
+    }
+}
+
+if (!function_exists('highlight_file')) {
+    function highlight_file(string $filename, bool $return = false): string|bool {
+        return $return ? '' : false;
+    }
+}
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
@@ -7,3 +19,4 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
